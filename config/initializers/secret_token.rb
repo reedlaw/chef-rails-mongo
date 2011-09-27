623b308e2e4aa862ce7f74589e3776e4ecb8c4ca
Reed Law <reedlaw@gmail.com>
2011-09-27 17:42:56 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChefRailsMongo::Application.config.secret_token = '72ac116e27fd264f579a98fe3e76d481f78ddde93fa6f3d06a0e9959589ee3bfeb63b8a44ce0e758b3a4d95fa9ba27b201e25ab8a80095e0443c7b5dde57853e'
