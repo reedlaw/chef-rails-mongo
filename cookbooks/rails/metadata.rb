b55236cd57f002b7a05a0711887651a2df2f1bbb
Reed Law <reedlaw@gmail.com>
2011-09-29 10:51:03 -0400

    add metadata

--- cookbooks/rails/metadata.rb
+++ cookbooks/rails/metadata.rb
@@ -4,3 +4,7 @@ license          'Apache v2.0'
 description      "Installs/Configures rails"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+
+%w{ unicorn }.each do |cb|
+  depends cb
+end
